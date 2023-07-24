8aae2086c5ce4d5c1550b777d8e1ae4ffcaf83fb
Ali Asghar <alliasgher@gmail.com>
2023-07-24 07:59:41 -0500

    padding homepage

--- app/page.jsx
+++ app/page.jsx
@@ -10,11 +10,11 @@ const Home = () => {
     <div className="hero min-h-screen bg-base-200">
   <div className="hero-content text-center">
     <div className="max-w-md">
-      <h1 className="text-5xl font-bold">CRUD Dashboard</h1>
+      <h1 className="text-5xl font-bold p-4">CRUD Dashboard</h1>
       <div className="flex flex-col w-full border-opacity-50">
-  <div className="grid h-20 card bg-blue-300 rounded-box place-items-center btn" onClick={() => {router.push("form")}}>Click to add a user.</div>
+  <div className="grid h-20 card bg-blue-300 rounded-box place-items-center btn p-4" onClick={() => {router.push("form")}}>Click to add a user.</div>
   <div className="divider">OR</div>
-  <div className="grid h-20 card bg-green-300 rounded-box place-items-center btn" onClick={() => {router.push("form/users")}}>Click to see exisiting users.</div>
+  <div className="grid h-20 card bg-green-300 rounded-box place-items-center btn p-4" onClick={() => {router.push("form/users")}}>Click to see exisiting users.</div>
 </div>
     </div>
   </div>
